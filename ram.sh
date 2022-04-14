27ef1b84d9aef7956540126f62face1a893d4fd9
Alessandro Braun <ablenda@gmail.com>
2022-04-14 13:24:48 -0300

    Ferramenta para atualizar data e hora.

---
+++ ram.sh
@@ -0,0 +1,15 @@
+#!user/bin/env bash
+
+#Author: Alessandro Braun ablenda@gmail.com
+#Discribe: Script para atualização de data e hora.
+#Version: 0.2
+#License: GPL
+
+  echo "------ESVAZIAR CACHE DE MEMÓRIA------"
+
+    sudo sync && sudo sysctl -w vm.drop_caches=3
+    sudo sync; sudo echo 3 > /proc/sys/vm/drop_caches
+    sudo sync; sudo echo 2 > /proc/sys/vm/drop_caches
+    sudo sync; sudo echo 1 > /proc/sys/vm/drop_caches
+
+  echo "------FIM DO PROCESSO------"
