fa645d05002a9fcaf0ba2935ccdab1048709329a
abraun79 <albraun79@outlook.com>
2024-09-25 13:33:48 +0000

    Changes made to avoid errors in the script

--- samba.sh
+++ samba.sh
@@ -4,8 +4,8 @@
 samba_server="fileserver.local"
 samba_share="//fileserver.local/data/backup"
 samba_mount="/mnt/backup"
-username="alessandro"  # Substitua pelo seu nome de usuário do Samba
-password="ale#0405"   # Substitua pela sua senha do Samba
+username="user"  # Substitua pelo seu nome de usuário do Samba
+password="password"   # Substitua pela sua senha do Samba
 
 # Montando o compartilhamento Samba
 if ! mount -t cifs "$samba_share" "$samba_mount" -o username="$username",password="$password"; then
